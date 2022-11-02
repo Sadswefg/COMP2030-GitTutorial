7332df2fc3cb7fed2e977fdeecc8b4864c170e89
Vu Nguyen <91305489+Sadswefg@users.noreply.github.com>
2022-11-02 11:17:54 +0700

    Update fib.py

--- fib.py
+++ fib.py
@@ -8,6 +8,8 @@ def fibonacci(position):
   if(position == 1 or position == 2):
     return 1
   if(position == 0):
-    return 0
-return fibonacci(position - 1) + fibonacci(position - 2)
+    return position
+  if(position < 0):
+    return None  
+  return fibonacci(position - 1) + fibonacci(position - 2)
 
